fea8d2127bf639074f018aed23ea48d1e45d8044
Bianca0104 <bianca.ioana.bolca@gmail.com>
2025-03-31 07:28:52 +0300

    Profile-1

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
+using StocksApp;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -26,7 +27,7 @@ namespace StockApp
         public MainWindow()
         {
             this.InitializeComponent();
-            rootFrame.Navigate(typeof(MainPage), null);
+            rootFrame.Navigate(typeof(ProfilePage), null);
             // rootFrame.Navigate(typeof(Test.TestPage), null);
         }
 
