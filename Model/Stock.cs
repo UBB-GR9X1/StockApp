9641d837a81898528f2951de69eedba85503aa89
OsakiTsukiko <osakitsukiko.mail@gmail.com>
2025-03-31 04:34:06 +0300

    update Model

---
+++ Model/Stock.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockApp.Model
+{
+    class Stock : BaseStock
+    {
+        private int _price;
+
+        public Stock(string name, string symbol, string author_cnp, int price) : base(name, symbol, author_cnp)
+        {
+            _price = price;
+        }
+        public int Price
+        {
+            get { return _price; }
+            set { _price = value; }
+        }
+    }
+}
