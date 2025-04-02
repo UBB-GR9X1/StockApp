ce459bcf888cda67f9c0dda1baaebe988b4b678c
OsakiTsukiko <osakitsukiko.mail@gmail.com>
2025-04-02 22:29:02 +0300

    fill database

--- StockPage/StockPageRepository.cs
+++ StockPage/StockPageRepository.cs
@@ -1,6 +1,7 @@
-﻿using StocksHomepage.Model;
+﻿using StockApp.StockPage.Model;
 using System;
 using System.Collections.Generic;
+using System.Data.SQLite;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,11 +11,25 @@ namespace StockApp.StockPage
     class StockPageRepository
     {
         private Database.DatabaseHelper databaseHelper;
+        private string cnp;
 
         public StockPageRepository()
         {
             databaseHelper = Database.DatabaseHelper.Instance;
-            Console.WriteLine("KMSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS");
+
+            SQLiteCommand getCNP = new SQLiteCommand("SELECT * FROM HARDCODED_CNPS", databaseHelper.GetConnection());
+            SQLiteDataReader reader = getCNP.ExecuteReader();
+            reader.Read();
+            this.cnp = reader["CNP"].ToString();
+        }
+
+        public Stock GetStock(string stockName)
+        {
+            SQLiteCommand getStock = new SQLiteCommand("SELECT * FROM STOCK WHERE STOCK_NAME = @name", databaseHelper.GetConnection());
+            getStock.Parameters.AddWithValue("@name", stockName);
+            SQLiteDataReader reader = getStock.ExecuteReader();
+            reader.Read();
+            return new Stock(reader["STOCK_NAME"].ToString(), reader["STOCK_SYMBOL"].ToString(), reader["AUTHOR_CNP"].ToString());
         }
     }
 }
---
+++ StockPage/StockPageService.cs
@@ -0,0 +1,30 @@
+﻿using StockApp.StockPage.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockApp.StockPage
+{
+    class StockPageService
+    {
+        StockPageRepository _repo;
+        Stock _stock;
+
+        public StockPageService(string stock_name)
+        {
+            _repo = new StockPageRepository();
+            _stock = _repo.GetStock(stock_name);
+        }
+
+        public string GetStockName()
+        {
+            return _stock.Name;
+        }
+        public string GetStockSymbol()
+        {
+            return _stock.Symbol;
+        }
+    }
+}
