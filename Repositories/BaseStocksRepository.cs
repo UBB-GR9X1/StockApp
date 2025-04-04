d00e8dbc8dea7349a9c90d169389f85281946745
Ionut253 <48173899+Ionut253@users.noreply.github.com>
2025-04-04 10:25:42 +0300

    Added random price to STOCK_VALUE table

--- Repositories/BaseStocksRepository.cs
+++ Repositories/BaseStocksRepository.cs
@@ -16,30 +16,54 @@ namespace StockApp.Repositories
             LoadStocks();
         }
 
-        public void AddStock(BaseStock stock)
+        public void AddStock(BaseStock stock, int initialPrice = 100)
         {
-            string checkQuery = "SELECT COUNT(*) FROM STOCK WHERE STOCK_NAME = @StockName";
-            using (var checkCommand = new SqlCommand(checkQuery, dbConnection))
+            // Start a transaction to ensure both operations complete successfully
+            using (var transaction = dbConnection.BeginTransaction())
             {
-                checkCommand.Parameters.AddWithValue("@StockName", stock.Name);
-                int count = Convert.ToInt32(checkCommand.ExecuteScalar());
-
-                if (count > 0)
+                try
                 {
-                    throw new Exception("A stock with this name already exists!");
+                    string checkQuery = "SELECT COUNT(*) FROM STOCK WHERE STOCK_NAME = @StockName";
+                    using (var checkCommand = new SqlCommand(checkQuery, dbConnection, transaction))
+                    {
+                        checkCommand.Parameters.AddWithValue("@StockName", stock.Name);
+                        int count = Convert.ToInt32(checkCommand.ExecuteScalar());
+                        if (count > 0)
+                        {
+                            throw new Exception("A stock with this name already exists!");
+                        }
+                    }
+
+                    // Insert the stock
+                    string stockQuery = "INSERT INTO STOCK (STOCK_NAME, STOCK_SYMBOL, AUTHOR_CNP) VALUES (@StockName, @StockSymbol, @AuthorCNP)";
+                    using (var stockCommand = new SqlCommand(stockQuery, dbConnection, transaction))
+                    {
+                        stockCommand.Parameters.AddWithValue("@StockName", stock.Name);
+                        stockCommand.Parameters.AddWithValue("@StockSymbol", stock.Symbol);
+                        stockCommand.Parameters.AddWithValue("@AuthorCNP", stock.AuthorCNP);
+                        stockCommand.ExecuteNonQuery();
+                    }
+
+                    // Insert the initial stock value
+                    string valueQuery = "INSERT INTO STOCK_VALUE (STOCK_NAME, PRICE) VALUES (@StockName, @Price)";
+                    using (var valueCommand = new SqlCommand(valueQuery, dbConnection, transaction))
+                    {
+                        valueCommand.Parameters.AddWithValue("@StockName", stock.Name);
+                        valueCommand.Parameters.AddWithValue("@Price", initialPrice);
+                        valueCommand.ExecuteNonQuery();
+                    }
+
+                    // Commit the transaction
+                    transaction.Commit();
+                    stocks.Add(stock);
+                }
+                catch (Exception ex)
+                {
+                    // Roll back transaction if something goes wrong
+                    transaction.Rollback();
+                    throw new Exception($"Failed to add stock: {ex.Message}");
                 }
             }
-
-            string query = "INSERT INTO STOCK (STOCK_NAME, STOCK_SYMBOL, AUTHOR_CNP) VALUES (@StockName, @StockSymbol, @AuthorCNP)";
-            using (var command = new SqlCommand(query, dbConnection))
-            {
-                command.Parameters.AddWithValue("@StockName", stock.Name);
-                command.Parameters.AddWithValue("@StockSymbol", stock.Symbol);
-                command.Parameters.AddWithValue("@AuthorCNP", stock.AuthorCNP);
-                command.ExecuteNonQuery();
-            }
-
-            stocks.Add(stock);
         }
 
         public void LoadStocks()
@@ -54,14 +78,12 @@ namespace StockApp.Repositories
                         var stockName = reader["STOCK_NAME"]?.ToString() ?? string.Empty;
                         var stockSymbol = reader["STOCK_SYMBOL"]?.ToString() ?? string.Empty;
                         var authorCnp = reader["AUTHOR_CNP"]?.ToString() ?? string.Empty;
-
                         var stock = new BaseStock(stockName, stockSymbol, authorCnp);
                         stocks.Add(stock);
                     }
                 }
             }
         }
-
 
         public List<BaseStock> GetAllStocks()
         {
