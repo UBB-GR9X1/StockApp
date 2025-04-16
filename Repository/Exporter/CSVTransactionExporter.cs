8a6863c8cb9f661769353057c7bfe6b6b96d377c
AdrianMosnegutu <adrianmosnegutuioan@yahoo.com>
2025-04-16 23:27:21 +0300

    Refactor transaction exporter classes for clarity
    Consolidate namespaces, improve string construction, and simplify StreamWriter instantiation. Introduce static JsonSerializerOptions in JSONTransactionExporter for better performance. These changes enhance readability and adhere to modern C# practices.

--- Repository/Exporter/CSVTransactionExporter.cs
+++ Repository/Exporter/CSVTransactionExporter.cs
@@ -1,24 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using StockApp.Model;
-
-namespace StockApp.Repository.Repository.Exporter
+﻿namespace StockApp.Repository.Exporter
 {
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.IO;
+    using StockApp.Models;
+
     public class CSVTransactionExporter : ITransactionExporter
     {
         public void Export(List<TransactionLogTransaction> transactions, string filePath)
         {
-            using var writer = new StreamWriter(filePath);
+            using StreamWriter writer = new (filePath);
             writer.WriteLine("StockSymbol,StockName,TransactionType,Amount,PricePerStock,TotalValue,Date,Author");
 
             foreach (var transaction in transactions)
             {
-                var csvRow = $"{transaction.StockSymbol},{transaction.StockName},{transaction.Type},{transaction.Amount},{transaction.PricePerStock},{transaction.TotalValue},{transaction.Date.ToString("yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture)},{transaction.Author}";
+                var csvRow =
+                    $"{transaction.StockSymbol}," +
+                    $"{transaction.StockName}," +
+                    $"{transaction.Type}," +
+                    $"{transaction.Amount}," +
+                    $"{transaction.PricePerStock}," +
+                    $"{transaction.TotalValue}," +
+                    $"{transaction.Date.ToString("yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture)}," +
+                    $"{transaction.Author}";
+
                 writer.WriteLine(csvRow);
             }
         }
--- Repository/Exporter/JSONTransactionExporter.cs
+++ Repository/Exporter/JSONTransactionExporter.cs
@@ -1,15 +1,17 @@
-﻿using System.Collections.Generic;
-using System.IO;
-using System.Text.Json;
-using StockApp.Model;
-
-namespace StockApp.Repository.Repository.Exporter
+﻿namespace StockApp.Repository.Exporter
 {
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Text.Json;
+    using StockApp.Models;
+
     public class JSONTransactionExporter : ITransactionExporter
     {
+        private static readonly JsonSerializerOptions Options = new () { WriteIndented = true };
+
         public void Export(List<TransactionLogTransaction> transactions, string filePath)
         {
-            var jsonData = JsonSerializer.Serialize(transactions, new JsonSerializerOptions { WriteIndented = true });
+            var jsonData = JsonSerializer.Serialize(transactions, Options);
             File.WriteAllText(filePath, jsonData);
         }
     }
