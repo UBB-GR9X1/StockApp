b22e8f6ae80eecd06bcb6834755140107d1ab086
AdrianMosnegutu <adrianmosnegutuioan@yahoo.com>
2025-04-16 19:14:35 +0300

    Refactor StringToVisibilityConverter logic
    Updated the Convert method to invert the string check condition.
    Now returns Visibility.Collapsed for non-string values.
    Adjusted visibility logic to allow inversion based on the "Inverse" parameter.

--- Converters/StringToVisibilityConverter.cs
+++ Converters/StringToVisibilityConverter.cs
@@ -1,27 +1,29 @@
-﻿using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Data;
-using System;
-
-namespace StockApp.Converters
+﻿namespace StockApp.Converters
 {
-    public class StringToVisibilityConverter : IValueConverter
+    using System;
+    using Microsoft.UI.Xaml;
+    using Microsoft.UI.Xaml.Data;
+
+    public partial class StringToVisibilityConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, string language)
         {
-            if (value is string stringValue)
+            if (value is not string stringValue)
             {
-                bool isEmpty = string.IsNullOrEmpty(stringValue);
-
-                // if parameter is "Inverse", invert the logic
-                if (parameter is string param && param == "Inverse")
-                {
-                    return isEmpty ? Visibility.Visible : Visibility.Collapsed;
-                }
-
-                return isEmpty ? Visibility.Collapsed : Visibility.Visible;
+                return Visibility.Collapsed;
             }
 
-            return Visibility.Collapsed;
+            bool isEmpty = string.IsNullOrEmpty(stringValue);
+
+            // if parameter is "Inverse", invert the logic
+            if (parameter is string param && param == "Inverse")
+            {
+                isEmpty = !isEmpty;
+            }
+
+            return isEmpty
+                ? Visibility.Collapsed
+                : Visibility.Visible;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, string language)
