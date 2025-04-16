65a62660aef961ae9282c59e2380b7b66f41cabf
AdrianMosnegutu <adrianmosnegutuioan@yahoo.com>
2025-04-16 19:08:19 +0300

    Refactor ReadStatusToFontWeightConverter class
    Updated parameter names in Convert methods for clarity.
    Revised logic to determine font weight based on reading status.
    Removed unnecessary comments and improved code readability.

--- Converters/ReadStatusToFontWeightConverter.cs
+++ Converters/ReadStatusToFontWeightConverter.cs
@@ -1,26 +1,27 @@
-﻿using Microsoft.UI.Text;
-using Microsoft.UI.Xaml.Data;
-using System;
-
-namespace StockApp.Converters
+﻿namespace StockApp.Converters
 {
-    public class ReadStatusToFontWeightConverter : IValueConverter
+    using System;
+    using Microsoft.UI.Text;
+    using Microsoft.UI.Xaml.Data;
+
+    public partial class ReadStatusToFontWeightConverter : IValueConverter
     {
-        public object Convert(object value, Type targetType, object parameter, string language)
+        public object Convert(object initialValue, Type targetType, object parameter, string language)
         {
-            // if read return normal, else return semi bold font for article title
-            if (value is bool isRead)
+            if (initialValue is not bool isReading)
             {
-                return isRead ? FontWeights.Normal : FontWeights.SemiBold;
+                return FontWeights.Normal;
             }
-            
-            return FontWeights.Normal;
+
+            // if read return normal, else return semi bold font for article title
+            return isReading
+                ? FontWeights.Normal
+                : FontWeights.SemiBold;
         }
-        
-        public object ConvertBack(object value, Type targetType, object parameter, string language)
+
+        public object ConvertBack(object initialValue, Type targetType, object parameter, string language)
         {
             throw new NotImplementedException();
         }
     }
 }
-
