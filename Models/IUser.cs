e95e55b8bbeed4edb13b2d64224033f151f6ccf9
Chris91ss <trillcriss@gmail.com>
2025-04-17 22:07:08 +0300

    Add IUser interface and refactor User class to implement it

---
+++ Models/IUser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockApp.Models
+{
+    public interface IUser
+    {
+        string Cnp { get; set; }
+
+        string Username { get; set; }
+
+        string Description { get; set; }
+
+        bool IsModerator { get; set; }
+
+        string Image { get; set; }
+
+        bool IsHidden { get; set; }
+    }
+}
