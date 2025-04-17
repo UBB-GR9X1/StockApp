842526a938ce5df900b6c6d149d12a6151ff4cb7
Chris91ss <trillcriss@gmail.com>
2025-04-17 21:42:32 +0300

    Create IHomepageStock interface and refactor HomepageStock to implement it

--- Models/HomepageStock.cs
+++ Models/HomepageStock.cs
@@ -3,22 +3,24 @@
     using Microsoft.UI;
     using Microsoft.UI.Xaml.Media;
 
-    public class HomepageStock()
+    public class HomepageStock : IHomepageStock
     {
-        public string Symbol { get; set; }
+        public string Symbol { get; set; } = string.Empty;
 
-        public string Name { get; set; }
+        public string Name { get; set; } = string.Empty;
 
         public int Price { get; set; }
 
-        public string Change { get; set; }
+        public string Change { get; set; } = string.Empty;
 
         public bool IsFavorite { get; set; }
 
-        public SolidColorBrush ChangeColor => this.Change.StartsWith('+')
+        public SolidColorBrush ChangeColor
+            => Change.StartsWith('+')
                 ? new SolidColorBrush(Colors.Green)
                 : new SolidColorBrush(Colors.Red);
 
-        public string FavoriteStar => this.IsFavorite ? "★" : "☆";
+        public string FavoriteStar
+            => IsFavorite ? "★" : "☆";
     }
 }
---
+++ Models/IHomepageStock.cs
@@ -0,0 +1,21 @@
+﻿namespace StockApp.Models
+{
+    using Microsoft.UI.Xaml.Media;
+
+    public interface IHomepageStock
+    {
+        string Symbol { get; set; }
+
+        string Name { get; set; }
+
+        int Price { get; set; }
+
+        string Change { get; set; }
+
+        bool IsFavorite { get; set; }
+
+        SolidColorBrush ChangeColor { get; }
+
+        string FavoriteStar { get; }
+    }
+}
