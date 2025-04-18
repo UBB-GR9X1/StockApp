c0ad3cbbd6e73aa0be33bb05197431a4b32b7daa
Pascu-Victor <me@vpascu.ro>
2025-04-18 21:36:54 +0300

    fix: merge clash resolution

--- Services/IStockPageService.cs
+++ Services/IStockPageService.cs
@@ -3,25 +3,31 @@
     using System.Collections.Generic;
     using StockApp.Models;
 
-namespace StockApp.Services
-{
     internal interface IStockPageService
     {
         bool BuyStock(int quantity);
+
         bool GetFavorite();
+
         int GetOwnedStocks();
+
         User GetStockAuthor();
+
         List<int> GetStockHistory();
+
         string GetStockName();
+
         string GetStockSymbol();
+
         int GetUserBalance();
+
         bool IsGuest();
+
         void SelectStock(Stock stock);
+
         bool SellStock(int quantity);
+
         void ToggleFavorite(bool state);
 
-        User GetStockAuthor();
-
-        void SelectStock(Stock stock);
     }
 }
