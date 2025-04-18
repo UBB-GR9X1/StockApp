9535b7ccee6efedf7fbc7304db39fd5d373a4cf5
AdrianTrill <trillthemlgpro@yahoo.com>
2025-04-19 01:07:38 +0300

    Added test cases for ProfileRepository & StockPageRepository

---
+++ StockApp.Repository.Tests/ProfileRepositoryTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StockApp.Repositories;
+using System;
+using System.Reflection;
+using System.Runtime.Serialization;
+
+namespace StockApp.Repository.Tests
+{
+    [TestClass]
+    public class ProfileRepositoryTests
+    {
+        private ProfileRepository _repo;
+
+        [TestInitialize]
+        public void Init()
+        {
+            _repo = (ProfileRepository)FormatterServices.GetUninitializedObject(typeof(ProfileRepository));
+
+            typeof(ProfileRepository)
+                .GetField("loggedInUserCNP", BindingFlags.NonPublic | BindingFlags.Instance)
+                ?.SetValue(_repo, "1234567890123");
+        }
+
+        [TestMethod]
+        public void GenerateUsername_ReturnsRandomName()
+        {
+            var method = typeof(ProfileRepository).GetMethod("GenerateUsername")!;
+            var result = method.Invoke(_repo, null) as string;
+
+            Assert.IsFalse(string.IsNullOrWhiteSpace(result));
+            Assert.IsTrue(result.Contains("_") || result.Length > 5); 
+        }
+
+        [TestMethod]
+        public void CnpField_IsSetCorrectly()
+        {
+            var field = typeof(ProfileRepository)
+                .GetField("loggedInUserCNP", BindingFlags.NonPublic | BindingFlags.Instance)!
+                .GetValue(_repo);
+
+            Assert.AreEqual("1234567890123", field);
+        }
+    }
+}
---
+++ StockApp.Repository.Tests/StockPageRepositoryTests.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StockApp.Repositories;
+using System.Reflection;
+using System.Runtime.Serialization;
+
+namespace StockApp.Repository.Tests
+{
+    [TestClass]
+    public class StockPageRepositoryTests
+    {
+        private StockPageRepository _repo;
+
+        [TestInitialize]
+        public void Init()
+        {
+            _repo = (StockPageRepository)FormatterServices.GetUninitializedObject(typeof(StockPageRepository));
+
+            typeof(StockPageRepository)
+                .GetField("cnp", BindingFlags.Instance | BindingFlags.NonPublic)
+                ?.SetValue(_repo, "1234567890123");
+
+            typeof(StockPageRepository)
+                .GetField("<IsGuest>k__BackingField", BindingFlags.Instance | BindingFlags.NonPublic)
+                ?.SetValue(_repo, true);
+        }
+
+        [TestMethod]
+        public void Repo_IsGuest_CanBeSet()
+        {
+            var isGuestProp = typeof(StockPageRepository).GetProperty("IsGuest");
+            Assert.IsNotNull(isGuestProp);
+            Assert.IsTrue((bool)isGuestProp.GetValue(_repo));
+        }
+
+        [TestMethod]
+        public void Repo_CnpField_Exists()
+        {
+            var field = typeof(StockPageRepository)
+                .GetField("cnp", BindingFlags.Instance | BindingFlags.NonPublic);
+            Assert.IsNotNull(field);
+            Assert.AreEqual("1234567890123", field.GetValue(_repo));
+        }
+
+        [TestMethod]
+        public void UserField_CanBeSetAndRetrieved()
+        {
+            var userType = typeof(StockApp.Models.User);
+            var user = (StockApp.Models.User)FormatterServices.GetUninitializedObject(userType);
+
+            typeof(StockPageRepository)
+                .GetProperty("User")
+                ?.SetValue(_repo, user);
+
+            Assert.AreEqual(user, _repo.User);
+        }
+    }
+}
