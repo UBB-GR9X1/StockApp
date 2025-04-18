ec7dd24088972759d07bc77d5d44c48dc39b4924
VioreliaMotofelea <viorelia.motofelea123@gmail.com>
2025-04-18 13:39:17 +0300

    a little changes for better organizing the exceptions

---
+++ Exceptions/ServicePersistenceExceptions.cs
@@ -0,0 +1,34 @@
+﻿namespace StockApp.Exceptions
+{
+    using System;
+
+    public class AlertPersistenceException : Exception
+    {
+        public AlertPersistenceException(string message, Exception? innerException = null)
+            : base(message, innerException) { }
+    }
+
+    public class DuplicateStockException : Exception
+    {
+        public DuplicateStockException(string stockName)
+            : base($"A stock with the name '{stockName}' already exists.") { }
+    }
+
+    public class HomepageStockPersistenceException : Exception
+    {
+        public HomepageStockPersistenceException(string message, Exception? innerException = null)
+            : base(message, innerException) { }
+    }
+
+    public class NewsPersistenceException : Exception
+    {
+        public NewsPersistenceException(string message, Exception? innerException = null)
+            : base(message, innerException) { }
+    }
+
+    public class StockPersistenceException : Exception
+    {
+        public StockPersistenceException(string message, Exception innerException)
+            : base(message, innerException) { }
+    }
+}
