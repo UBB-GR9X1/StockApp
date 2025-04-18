d03e069fcf130348394cd3c65e779b4c6ce40ae1
AdrianTrill <trillthemlgpro@yahoo.com>
2025-04-19 00:02:52 +0300

    Added test cases for GemStoreRepository

---
+++ Repositories/IDbExecutor.cs
@@ -0,0 +1,12 @@
+﻿namespace StockApp.Repositories
+{
+    using System;
+    using Microsoft.Data.SqlClient;
+
+    public interface IDbExecutor
+    {
+        object? ExecuteScalar(string sql, Action<SqlCommand> parameterize);
+
+        void ExecuteNonQuery(string sql, Action<SqlCommand> parameterize);
+    }
+}
