bbf1290e79a07a561ae02df1e68e77c482ffcd04
Sergiu236 <sergiumocan04@yahoo.com>
2025-04-19 16:47:41 +0300

    Comment from command: generic and stocks

--- Commands/RelayCommandGeneric.cs
+++ Commands/RelayCommandGeneric.cs
@@ -8,14 +8,28 @@
         private readonly Action<T> execute;
         private readonly Func<T, bool>? canExecute;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RelayCommandGeneric{T}"/> class.
+        /// </summary>
         public event EventHandler CanExecuteChanged;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RelayCommandGeneric{T}"/> class.
+        /// </summary>
+        /// <param name="execute"></param>
+        /// <param name="canExecute"></param>
+        /// <exception cref="ArgumentNullException"></exception>
         public RelayCommandGeneric(Action<T> execute, Func<T, bool>? canExecute = null)
         {
             this.execute = execute ?? throw new ArgumentNullException(nameof(execute));
             this.canExecute = canExecute;
         }
 
+        /// <summary>
+        /// Determines whether the command can execute in its current state.
+        /// </summary>
+        /// <param name="parameter"></param>
+        /// <returns></returns>
         public bool CanExecute(object parameter)
         {
             if (parameter == null && typeof(T).IsValueType)
@@ -26,8 +40,15 @@
             return this.canExecute == null || this.canExecute((T)parameter);
         }
 
+        /// <summary>
+        /// Invokes the <see cref="Execute"/> method on the command.
+        /// </summary>
+        /// <param name="parameter"></param>
         public void Execute(object parameter) => this.execute((T)parameter);
 
+        /// <summary>
+        /// Raises the <see cref="CanExecuteChanged"/> event to indicate that the command's ability to execute has changed.
+        /// </summary>
         public void RaiseCanExecuteChanged() => this.CanExecuteChanged?.Invoke(this, EventArgs.Empty);
     }
 }
