b27c55d8941da032fbbd0d8d6d07eff833a216dd
Sergiu236 <sergiumocan04@yahoo.com>
2025-04-19 16:52:02 +0300

    Comment from vies: article and homepage

--- Views/ArticleCreationView.xaml.cs
+++ Views/ArticleCreationView.xaml.cs
@@ -5,6 +5,10 @@ namespace StockApp.Views
 
     public sealed partial class ArticleCreationView : Page
     {
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ArticleCreationView"/> class.
+        /// </summary>
         public ArticleCreationView()
         {
             this.InitializeComponent();
@@ -12,8 +16,16 @@ namespace StockApp.Views
             this.Loaded += this.OnLoaded;
         }
 
+        /// <summary>
+        /// Gets the view model for the article creation view.
+        /// </summary>
         public ArticleCreationViewModel ViewModel { get; } = new ();
 
+        /// <summary>
+        /// Handles the Loaded event of the ArticleCreationView control.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void OnLoaded(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
         {
             this.ViewModel.Initialize();
