ee60bbe57a22cf3623f6acdbd0f12578329b163d
Sergiu236 <sergiumocan04@yahoo.com>
2025-04-19 15:24:29 +0300

    Comment StockPageViewModel.cs

--- ViewModels/StockPageViewModel.cs
+++ ViewModels/StockPageViewModel.cs
@@ -16,7 +16,10 @@
     using StockApp.Models;
     using StockApp.Services;
 
-    class StockPageViewModel : INotifyPropertyChanged
+    /// <summary>
+    /// ViewModel managing data and operations for the stock detail page.
+    /// </summary>
+    public class StockPageViewModel : INotifyPropertyChanged
     {
         private string _stockName;
         private string _stockSymbol;
@@ -33,7 +36,15 @@
         private ITextBlock _ownedStocks;
         private IChart _stockChart;
 
-
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StockPageViewModel"/> class with dependencies.
+        /// </summary>
+        /// <param name="service">Service for retrieving stock data.</param>
+        /// <param name="selectedStock">The stock object selected by the user.</param>
+        /// <param name="priceLabel">Adapter for displaying the current price.</param>
+        /// <param name="increaseLabel">Adapter for displaying price change percentage.</param>
+        /// <param name="ownedStocks">Adapter for displaying number of owned stocks.</param>
+        /// <param name="stockChart">Adapter for displaying the stock history chart.</param>
         public StockPageViewModel(
             IStockPageService service,
             Stock selectedStock,
@@ -56,34 +67,51 @@
             this.IsFavorite = this.stockPageService.GetFavorite();
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StockPageViewModel"/> class with default adapters.
+        /// </summary>
+        /// <param name="selectedStock">The stock object selected by the user.</param>
+        /// <param name="priceLabel">TextBlock control for price display.</param>
+        /// <param name="increaseLabel">TextBlock control for percentage change display.</param>
+        /// <param name="ownedStocks">TextBlock control for owned stocks display.</param>
+        /// <param name="stockChart">CartesianChart control for history display.</param>
         public StockPageViewModel(
             Stock selectedStock,
             TextBlock priceLabel,
             TextBlock increaseLabel,
             TextBlock ownedStocks,
             CartesianChart stockChart)
-          : this(
-        new StockPageService(),
-        selectedStock,
-        new TextBlockAdapter(priceLabel),
-        new TextBlockAdapter(increaseLabel),
-        new TextBlockAdapter(ownedStocks),
-        new ChartAdapter(stockChart))
-        { }
+            : this(
+                  new StockPageService(),
+                  selectedStock,
+                  new TextBlockAdapter(priceLabel),
+                  new TextBlockAdapter(increaseLabel),
+                  new TextBlockAdapter(ownedStocks),
+                  new ChartAdapter(stockChart))
+        {
+        }
 
+        /// <summary>
+        /// Updates all displayed stock values, including price, change percentage, owned count, and chart.
+        /// </summary>
         public void UpdateStockValue()
         {
             if (!this.stockPageService.IsGuest())
             {
+                // Inline: update user gems and owned stock count
                 this.UserGems = this.stockPageService.GetUserBalance();
                 this._ownedStocks.Text = "Owned: " + this.stockPageService.GetOwnedStocks().ToString();
             }
             List<int> stockHistory = this.stockPageService.GetStockHistory();
+            // Inline: display latest price
             this._priceLabel.Text = stockHistory.Last().ToString() + " ❇️ Gems";
             if (stockHistory.Count > 1)
             {
-                int increasePerc = (stockHistory.Last() - stockHistory[stockHistory.Count - 2]) * 100 / stockHistory[stockHistory.Count - 2];
+                // Inline: calculate percentage change from previous value
+                int increasePerc = (stockHistory.Last() - stockHistory[stockHistory.Count - 2]) * 100
+                                    / stockHistory[stockHistory.Count - 2];
                 this._increaseLabel.Text = increasePerc + "%";
+                // Inline: set color based on positive or negative change
                 if (increasePerc > 0)
                 {
                     this._increaseLabel.Foreground = new SolidColorBrush(Colors.Green);
@@ -93,6 +121,7 @@
                     this._increaseLabel.Foreground = new SolidColorBrush(Colors.IndianRed);
                 }
             }
+            // Inline: update chart layout and series data
             this._stockChart.UpdateLayout();
             this._stockChart.Series = new ISeries[]
             {
@@ -100,44 +129,36 @@
                 {
                     Values = stockHistory.TakeLast(30).ToArray(),
                     Fill = null,
-                    Stroke = new SolidColorPaint(SKColor.Parse("#4169E1"), 5),
+                    Stroke = new SolidColorPaint(SKColor.Parse("#4169E1"), 5), // FIXME: make stroke color configurable
                     GeometryStroke = new SolidColorPaint(SKColor.Parse("#4169E1"), 5),
-
                 }
             };
+            // TODO: handle case where stockHistory is empty to prevent exceptions
         }
 
+        /// <summary>
+        /// Gets or sets a value indicating whether this stock is in the user's favorites.
+        /// </summary>
         public bool IsFavorite
         {
-            get
-            {
-                return this._isFavorite;
-            }
-
+            get => this._isFavorite;
             set
             {
                 if (this._isFavorite == value) return;
                 this._isFavorite = value;
                 this.stockPageService.ToggleFavorite(this._isFavorite);
-                if (this._isFavorite)
-                {
-                    this.FavoriteButtonColor = "#ff0000"; // Red color for favorite
-                }
-                else
-                {
-                    this.FavoriteButtonColor = "#ffff5c"; // Default color
-                }
+                // Inline: update button color based on favorite state
+                this.FavoriteButtonColor = this._isFavorite ? "#ff0000" : "#ffff5c";
                 this.OnPropertyChanged(nameof(this.IsFavorite));
             }
         }
 
+        /// <summary>
+        /// Gets or sets the color of the favorite button.
+        /// </summary>
         public string FavoriteButtonColor
         {
-            get
-            {
-                return this._favoriteButtonColor;
-            }
-
+            get => this._favoriteButtonColor;
             set
             {
                 this._favoriteButtonColor = value;
@@ -145,13 +166,12 @@
             }
         }
 
+        /// <summary>
+        /// Gets or sets the display name of the stock.
+        /// </summary>
         public string StockName
         {
-            get
-            {
-                return this._stockName;
-            }
-
+            get => this._stockName;
             set
             {
                 if (this._stockName != value)
@@ -162,13 +182,12 @@
             }
         }
 
+        /// <summary>
+        /// Gets or sets the trading symbol of the stock.
+        /// </summary>
         public string StockSymbol
         {
-            get
-            {
-                return this._stockSymbol;
-            }
-
+            get => this._stockSymbol;
             set
             {
                 if (this._stockSymbol != value)
@@ -179,40 +198,40 @@
             }
         }
 
+        /// <summary>
+        /// Occurs when a property value changes.
+        /// </summary>
         public event PropertyChangedEventHandler PropertyChanged;
 
-        protected void OnPropertyChanged(string propertyName)
-        {
-            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-
+        /// <summary>
+        /// Toggles the favorite state of the stock.
+        /// </summary>
         public void ToggleFavorite()
         {
             this.IsFavorite = !this.IsFavorite;
         }
 
+        /// <summary>
+        /// Gets or sets a value indicating whether the user is a guest.
+        /// </summary>
         public bool IsGuest
         {
-            get
-            {
-                return this._isGuest;
-            }
-
+            get => this._isGuest;
             set
             {
                 this._isGuest = value;
+                // Inline: update UI visibility based on guest status
                 this.GuestVisibility = this._isGuest ? "Collapsed" : "Visible";
                 this.OnPropertyChanged(nameof(this.IsGuest));
             }
         }
 
+        /// <summary>
+        /// Gets or sets the visibility of guest-only UI elements.
+        /// </summary>
         public string GuestVisibility
         {
-            get
-            {
-                return this._guestVisibility;
-            }
-
+            get => this._guestVisibility;
             set
             {
                 this._guestVisibility = value;
@@ -220,28 +239,27 @@
             }
         }
 
+        /// <summary>
+        /// Gets or sets the user's current gem balance.
+        /// </summary>
         public int UserGems
         {
-            get
-            {
-                return this._userGems;
-            }
-
+            get => this._userGems;
             set
             {
                 this._userGems = value;
+                // Inline: update text to reflect gem count
                 this.UserGemsText = $"{this._userGems} ❇️ Gems";
                 this.OnPropertyChanged(nameof(this.UserGems));
             }
         }
 
+        /// <summary>
+        /// Gets or sets the display text for the user's gem balance.
+        /// </summary>
         public string UserGemsText
         {
-            get
-            {
-                return this._userGemsText;
-            }
-
+            get => this._userGemsText;
             set
             {
                 this._userGemsText = value;
@@ -249,6 +267,11 @@
             }
         }
 
+        /// <summary>
+        /// Attempts to buy a specified quantity of the stock.
+        /// </summary>
+        /// <param name="quantity">The number of shares to buy.</param>
+        /// <returns><c>true</c> if the purchase succeeded; otherwise, <c>false</c>.</returns>
         public bool BuyStock(int quantity)
         {
             bool res = this.stockPageService.BuyStock(quantity);
@@ -256,6 +279,11 @@
             return res;
         }
 
+        /// <summary>
+        /// Attempts to sell a specified quantity of the stock.
+        /// </summary>
+        /// <param name="quantity">The number of shares to sell.</param>
+        /// <returns><c>true</c> if the sale succeeded; otherwise, <c>false</c>.</returns>
         public bool SellStock(int quantity)
         {
             bool res = this.stockPageService.SellStock(quantity);
@@ -263,9 +291,22 @@
             return res;
         }
 
+        /// <summary>
+        /// Gets the author (owner) of the stock.
+        /// </summary>
+        /// <returns>A <see cref="User"/> object representing the author.</returns>
         public User GetStockAuthor()
         {
             return this.stockPageService.GetStockAuthor();
+        }
+
+        /// <summary>
+        /// Raises the <see cref="PropertyChanged"/> event.
+        /// </summary>
+        /// <param name="propertyName">Name of the property that changed.</param>
+        protected void OnPropertyChanged(string propertyName)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
