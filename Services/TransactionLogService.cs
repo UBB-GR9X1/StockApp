716e55a7ecb1f41d580228cedd2aaeee9c8fad60
Sergiu236 <sergiumocan04@yahoo.com>
2025-04-19 16:42:35 +0300

    Comment from service: log

--- Services/TransactionLogService.cs
+++ Services/TransactionLogService.cs
@@ -12,17 +12,35 @@
     {
         private readonly ITransactionRepository transactionRepository;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TransactionLogService"/> class.
+        /// </summary>
+        /// <param name="transactionRepository"></param>
+        /// <exception cref="ArgumentNullException"></exception>
         public TransactionLogService(ITransactionRepository transactionRepository)
         {
             this.transactionRepository = transactionRepository ?? throw new ArgumentNullException(nameof(transactionRepository));
         }
 
+        /// <summary>
+        /// Retrieves all transactions for the current user.
+        /// </summary>
+        /// <param name="criteria"></param>
+        /// <returns></returns>
         public List<TransactionLogTransaction> GetFilteredTransactions(TransactionFilterCriteria criteria)
         {
             criteria.Validate();
             return transactionRepository.GetByFilterCriteria(criteria);
         }
 
+        /// <summary>
+        /// Sorts a list of transactions based on the specified criteria.
+        /// </summary>
+        /// <param name="transactions"></param>
+        /// <param name="sortType"></param>
+        /// <param name="ascending"></param>
+        /// <returns></returns>
+        /// <exception cref="InvalidSortTypeException"></exception>
         public List<TransactionLogTransaction> SortTransactions(List<TransactionLogTransaction> transactions, string sortType = "Date", bool ascending = true)
         {
             return sortType switch
@@ -43,6 +61,15 @@
             };
         }
 
+        /// <summary>
+        /// Exports a list of transactions to a specified file format.
+        /// </summary>
+        /// <param name="transactions"></param>
+        /// <param name="filePath"></param>
+        /// <param name="format"></param>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="ArgumentException"></exception>
+        /// <exception cref="ExportFormatNotSupportedException"></exception>
         public void ExportTransactions(List<TransactionLogTransaction> transactions, string filePath, string format)
         {
             if (transactions == null)
