40d34470aa875b36986046b80208ce07931ab732
Sergiu236 <sergiumocan04@yahoo.com>
2025-04-19 16:34:08 +0300

    Comment from service: navigation and homepage

--- Services/HomepageService.cs
+++ Services/HomepageService.cs
@@ -8,11 +8,31 @@
     class HomepageService : IHomepageService
     {
         private readonly IHomepageStocksRepository _repo;
+
+        /// <summary>
+        /// Represents a collection of stocks displayed on the homepage.
+        /// </summary>
         public ObservableCollection<HomepageStock> FavoriteStocks { get; private set; }
+
+        /// <summary>
+        /// Represents a collection of all stocks available in the application.
+        /// </summary>
         public ObservableCollection<HomepageStock> AllStocks { get; private set; }
+
+        /// <summary>
+        /// Represents a collection of stocks filtered based on user input.
+        /// </summary>
         public ObservableCollection<HomepageStock> FilteredAllStocks { get; private set; }
+
+        /// <summary>
+        /// Represents a collection of favorite stocks filtered based on user input.
+        /// </summary>
         public ObservableCollection<HomepageStock> FilteredFavoriteStocks { get; private set; }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HomepageService"/> class.
+        /// </summary>
+        /// <param name="repo"></param>
         public HomepageService(IHomepageStocksRepository repo)
         {
             _repo = repo;
@@ -21,18 +41,33 @@
             FavoriteStocks = new ObservableCollection<HomepageStock>(stocks.Where(s => s.IsFavorite).ToList());
         }
 
+        /// <summary>
+        /// Default constructor for the <see cref="HomepageService"/> class.
+        /// </summary>
         public HomepageService() : this(new HomepageStocksRepository()) { }
 
+        /// <summary>
+        /// Returns a collection of stocks that are marked as favorites.
+        /// </summary>
+        /// <returns></returns>
         public ObservableCollection<HomepageStock> GetFavoriteStocks()
         {
             return FavoriteStocks;
         }
 
+        /// <summary>
+        /// Returns a collection of all stocks available in the application.
+        /// </summary>
+        /// <returns></returns>
         public ObservableCollection<HomepageStock> GetAllStocks()
         {
             return AllStocks;
         }
 
+        /// <summary>
+        /// Filters the stocks based on the provided query string.
+        /// </summary>
+        /// <param name="query"></param>
         public void FilterStocks(string query)
         {
             FilteredAllStocks = new ObservableCollection<HomepageStock>(AllStocks
@@ -46,6 +81,10 @@
                 .ToList());
         }
 
+        /// <summary>
+        /// Sorts the stocks based on the provided sort option.
+        /// </summary>
+        /// <param name="sortOption"></param>
         public void SortStocks(string sortOption)
         {
             if (FilteredAllStocks == null || FilteredFavoriteStocks == null)
@@ -91,6 +130,10 @@
             }
         }
 
+        /// <summary>
+        /// Removes a stock from the favorites list and updates its status.
+        /// </summary>
+        /// <param name="stock"></param>
         public void RemoveFromFavorites(HomepageStock stock)
         {
             _repo.RemoveFromFavorites(stock);
@@ -98,22 +141,38 @@
             stock.IsFavorite = false;
         }
 
+        /// <summary>
+        /// Adds a stock to the favorites list and updates its status.
+        /// </summary>
+        /// <param name="stock"></param>
         public void AddToFavorites(HomepageStock stock)
         {
             _repo.AddToFavorites(stock);
             FavoriteStocks.Add(stock);
             stock.IsFavorite = true;
         }
+
+        /// <summary>
+        /// Checks if the user is a guest user.
+        /// </summary>
+        /// <returns></returns>
         public bool IsGuestUser()
         {
             return _repo.IsGuestUser(_repo.GetUserCnp());
         }
 
+        /// <summary>
+        /// Returns the user's CNP (Personal Numeric Code).
+        /// </summary>
+        /// <returns></returns>
         public string GetUserCNP()
         {
             return _repo.GetUserCnp();
         }
 
+        /// <summary>
+        /// Creates a user profile if it doesn't exist.
+        /// </summary>
         public void CreateUserProfile()
         {
             _repo.CreateUserProfile();
--- Services/NavigationService.cs
+++ Services/NavigationService.cs
@@ -7,8 +7,14 @@
     {
         private static readonly Lazy<NavigationService> instance = new(() => new NavigationService());
 
+        /// <summary>
+        /// Singleton instance of the NavigationService.
+        /// </summary>
         public static NavigationService Instance => instance.Value;
 
+        /// <summary>
+        /// The root frame for navigation.
+        /// </summary>
         private static INavigationFrame RootFrame;
 
         // Private constructor to enforce singleton pattern
@@ -16,11 +22,23 @@
         {
         }
 
+        /// <summary>
+        /// Initializes the NavigationService with the specified frame.
+        /// </summary>
+        /// <param name="frame"></param>
+        /// <exception cref="ArgumentNullException"></exception>
         public static void Initialize(INavigationFrame frame)
         {
             RootFrame = frame ?? throw new ArgumentNullException(nameof(frame));
         }
 
+        /// <summary>
+        /// Navigates to the specified page type with an optional parameter.
+        /// </summary>
+        /// <param name="pageType"></param>
+        /// <param name="parameter"></param>
+        /// <returns></returns>
+        /// <exception cref="InvalidOperationException"></exception>
         public bool Navigate(Type pageType, object? parameter = null)
         {
             if (RootFrame == null)
@@ -31,6 +49,10 @@
             return RootFrame.Navigate(pageType, parameter);
         }
 
+        /// <summary>
+        /// Navigates back in the navigation stack.
+        /// </summary>
+        /// <exception cref="InvalidOperationException"></exception>
         public void GoBack()
         {
             if (RootFrame == null)
@@ -44,8 +66,16 @@
             }
         }
 
+        /// <summary>
+        /// Checks if the navigation stack can go back.
+        /// </summary>
         public bool CanGoBack => RootFrame?.CanGoBack ?? false;
 
+        /// <summary>
+        /// Navigates to the article detail page with the specified article ID.
+        /// </summary>
+        /// <param name="articleId"></param>
+        /// <exception cref="InvalidOperationException"></exception>
         public void NavigateToArticleDetail(string articleId)
         {
             if (RootFrame == null)
