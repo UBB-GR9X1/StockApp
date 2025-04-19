1de68cffd2c9abf34bc44f7c99b4086a4794d0c6
Sergiu236 <sergiumocan04@yahoo.com>
2025-04-19 16:45:03 +0300

    Comment from command: relay and command

--- Commands/RelayCommand.cs
+++ Commands/RelayCommand.cs
@@ -8,16 +8,34 @@
         private readonly Action<object> execute;
         private readonly Predicate<object>? canExecute;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RelayCommand"/> class.
+        /// </summary>
+        /// <param name="execute"></param>
+        /// <param name="canExecute"></param>
+        /// <exception cref="ArgumentNullException"></exception>
         public RelayCommand(Action<object> execute, Predicate<object>? canExecute = null)
         {
             this.execute = execute ?? throw new ArgumentNullException(nameof(execute));
             this.canExecute = canExecute;
         }
 
+        /// <summary>
+        /// Determines whether the command can execute in its current state.
+        /// </summary>
+        /// <param name="parameter"></param>
+        /// <returns></returns>
         public bool CanExecute(object parameter) => this.canExecute?.Invoke(parameter) ?? true;
 
+        /// <summary>
+        /// Invokes the <see cref="Execute"/> method on the command.
+        /// </summary>
+        /// <param name="parameter"></param>
         public void Execute(object parameter) => this.execute(parameter);
 
+        /// <summary>
+        /// Raises the <see cref="CanExecuteChanged"/> event to indicate that the command's ability to execute has changed.
+        /// </summary>
         public event EventHandler CanExecuteChanged
         {
             add => CommandManager.RequerySuggested += value;
