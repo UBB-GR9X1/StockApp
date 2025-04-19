877e1642f5cd3b07885f86c8fdec3f34c711132f
Sergiu236 <sergiumocan04@yahoo.com>
2025-04-19 16:50:23 +0300

    Comment from vies: admin and alert

--- Views/AlertsView.xaml.cs
+++ Views/AlertsView.xaml.cs
@@ -5,12 +5,18 @@ namespace StockApp.Views
 
     public sealed partial class AlertsView : Page
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AlertsView"/> class.
+        /// </summary>
         public AlertsView()
         {
             this.InitializeComponent();
             this.DataContext = this.ViewModel;
         }
 
+        /// <summary>
+        /// Gets the ViewModel for managing alerts.
+        /// </summary>
         public AlertViewModel ViewModel { get; } = new ();
     }
 }
