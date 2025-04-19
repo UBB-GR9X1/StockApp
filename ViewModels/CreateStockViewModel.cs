7a8b08ed0bd0069916f858c32cc54d2ea7f50756
Sergiu236 <sergiumocan04@yahoo.com>
2025-04-19 15:06:44 +0300

    Comment CreateStockViewModel.cs

--- ViewModels/CreateStockViewModel.cs
+++ ViewModels/CreateStockViewModel.cs
@@ -22,21 +22,19 @@ namespace StockApp.ViewModels
         private bool _isAdmin;
         private bool _isInputValid;
 
+        /// <summary>
+        /// Occurs when a property value changes.
+        /// </summary>
         public event PropertyChangedEventHandler? PropertyChanged;
+
+        /// <summary>
+        /// Gets the command to create a new stock entry.
+        /// </summary>
         public ICommand CreateStockCommand { get; }
 
-        public CreateStockViewModel(ICreateStockService stockService)
-        {
-            _stockService = stockService ?? throw new ArgumentNullException(nameof(stockService));
-            CreateStockCommand = new RelayCommand(CreateStock, CanCreateStock);
-            IsAdmin = CheckIfUserIsAdmin();
-        }
-
-        public CreateStockViewModel()
-            : this(new CreateStockService())
-        {
-        }
-
+        /// <summary>
+        /// Gets or sets a value indicating whether the current user is an administrator.
+        /// </summary>
         public bool IsAdmin
         {
             get => _isAdmin;
@@ -50,6 +48,9 @@ namespace StockApp.ViewModels
             }
         }
 
+        /// <summary>
+        /// Gets or sets the name of the stock to create.
+        /// </summary>
         public string StockName
         {
             get => _stockName;
@@ -64,6 +65,9 @@ namespace StockApp.ViewModels
             }
         }
 
+        /// <summary>
+        /// Gets or sets the symbol of the stock to create.
+        /// </summary>
         public string StockSymbol
         {
             get => _stockSymbol;
@@ -78,6 +82,9 @@ namespace StockApp.ViewModels
             }
         }
 
+        /// <summary>
+        /// Gets or sets the CNP identifier of the author.
+        /// </summary>
         public string AuthorCnp
         {
             get => _authorCnp;
@@ -92,6 +99,9 @@ namespace StockApp.ViewModels
             }
         }
 
+        /// <summary>
+        /// Gets or sets the validation or service message to display.
+        /// </summary>
         public string Message
         {
             get => _message;
@@ -105,6 +115,9 @@ namespace StockApp.ViewModels
             }
         }
 
+        /// <summary>
+        /// Gets a value indicating whether the current inputs are valid for creating a stock.
+        /// </summary>
         public bool IsInputValid
         {
             get => _isInputValid;
@@ -117,6 +130,21 @@ namespace StockApp.ViewModels
             }
         }
 
+        public CreateStockViewModel(ICreateStockService stockService)
+        {
+            _stockService = stockService ?? throw new ArgumentNullException(nameof(stockService));
+            CreateStockCommand = new RelayCommand(CreateStock, CanCreateStock);
+            IsAdmin = CheckIfUserIsAdmin();
+        }
+
+        public CreateStockViewModel()
+            : this(new CreateStockService())
+        {
+        }
+
+        /// <summary>
+        /// Validates all input fields and sets <see cref="IsInputValid"/> and <see cref="Message"/> accordingly.
+        /// </summary>
         private void ValidateInputs()
         {
             if (_suppressValidation) return;
@@ -124,6 +152,7 @@ namespace StockApp.ViewModels
             Message = string.Empty;
             IsInputValid = true;
 
+            // Validate stock name presence and format
             if (string.IsNullOrWhiteSpace(StockName))
             {
                 Message = "Stock Name is required!";
@@ -131,10 +160,12 @@ namespace StockApp.ViewModels
             }
             else if (!Regex.IsMatch(StockName, @"^[A-Za-z ]{1,20}$"))
             {
+                // Only letters and spaces, up to 20 characters
                 Message = "Stock Name must be max 20 characters and contain only letters & spaces!";
                 IsInputValid = false;
             }
 
+            // Validate stock symbol presence and format
             if (string.IsNullOrWhiteSpace(StockSymbol))
             {
                 Message = "Stock Symbol is required!";
@@ -142,10 +173,12 @@ namespace StockApp.ViewModels
             }
             else if (!Regex.IsMatch(StockSymbol, @"^[A-Za-z0-9]{1,5}$"))
             {
+                // Alphanumeric only, up to 5 characters
                 Message = "Stock Symbol must be alphanumeric and max 5 characters!";
                 IsInputValid = false;
             }
 
+            // Validate CNP presence and format
             if (string.IsNullOrWhiteSpace(AuthorCnp))
             {
                 Message = "Author CNP is required!";
@@ -153,6 +186,7 @@ namespace StockApp.ViewModels
             }
             else if (!Regex.IsMatch(AuthorCnp, @"^\d{13}$"))
             {
+                // Exactly 13 digits required
                 Message = "Author CNP must be exactly 13 digits!";
                 IsInputValid = false;
             }
@@ -168,23 +202,31 @@ namespace StockApp.ViewModels
 
             if (Message == "Stock added successfully with initial value!")
             {
+                // TODO: Clear inputs after successful creation without re-validating interim empty values
                 _suppressValidation = true;
-                StockName = "";
-                StockSymbol = "";
-                AuthorCnp = "";
+                StockName = string.Empty;
+                StockSymbol = string.Empty;
+                AuthorCnp = string.Empty;
                 _suppressValidation = false;
             }
         }
 
+        /// <summary>
+        /// Checks whether the current user has administrative privileges.
+        /// </summary>
+        /// <returns>True if the user is an admin; otherwise false.</returns>
         protected virtual bool CheckIfUserIsAdmin()
         {
-            // This method should check if the user is an admin.
-            // For now, let's assume the user is an admin.
+            // TODO: Implement real admin check logic instead of assuming guest status
             if (_stockService.CheckIfUserIsGuest())
                 Message = "You are a guest user and cannot create stocks!";
             return !_stockService.CheckIfUserIsGuest();
         }
 
+        /// <summary>
+        /// Raises the <see cref="PropertyChanged"/> event for the specified property.
+        /// </summary>
+        /// <param name="propertyName">Name of the property that changed. Auto-supplied if omitted.</param>
         protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
