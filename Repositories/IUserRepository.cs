16d4de72f98fabbbe3f9d3bc52dcbf970e9317f9
PandorAura <aurapandor@gmail.com>
2025-05-05 15:49:55 +0300

    Merge user repo

--- Repositories/IUserRepository.cs
+++ Repositories/IUserRepository.cs
@@ -1,5 +1,6 @@
 ﻿namespace StockApp.Repositories
 {
+    using System;
     using System.Collections.Generic;
     using System.Threading.Tasks;
     using StockApp.Models;
@@ -10,14 +11,24 @@
 
         Task CreateUserAsync(User user);
 
-        Task DeleteUserAsync(string cnp);
-
-        Task<List<User>> GetAllUsersAsync();
-
-        Task<User> GetUserByCnpAsync(string cnp);
+        Task<User> GetUserByCnpAsync(string userCNP);
 
         Task<User> GetUserByUsernameAsync(string username);
 
         Task UpdateUserAsync(User user);
+
+        Task DeleteUserAsync(string userCNP);
+
+        Task<List<User>> GetAllUsersAsync();
+
+        Task PenalizeUserAsync(string cnp, int penaltyAmount);
+
+        Task IncrementOffensesCountAsync(string cnp);
+
+        Task UpdateUserCreditScoreAsync(string cnp, int creditScore);
+
+        Task UpdateUserROIAsync(string cnp, decimal roi);
+
+        Task UpdateUserRiskScoreAsync(string cnp, int riskScore);
     }
 }
