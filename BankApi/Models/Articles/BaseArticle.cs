067e0126648ae3eecd22aac027dec755d312139b
AdrianMosnegutu <adrianmosnegutuioan@yahoo.com>
2025-05-08 00:59:16 +0300

    Add BaseArticle class and related enhancements
    Introduces a new `BaseArticle` class with common properties for articles, including `ArticleId`, `Title`, `Summary`, `Content`, `PublishedOn`, and `Status`. The `NewsArticle` and `UserArticle` classes inherit from `BaseArticle`, adding specific properties. A `Status` enum is defined for article review statuses, and a `StatusValidationAttribute` class is added for validation. These changes improve the structure and validation of article-related data in the application.

---
+++ BankApi/Models/Articles/BaseArticle.cs
@@ -0,0 +1,75 @@
+﻿namespace BankApi.Models.Articles
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using BankApi.Models;
+
+    /// <summary>
+    /// Represents the base article model containing common properties for articles.
+    /// </summary>
+    public class BaseArticle
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BaseArticle"/> class.
+        /// </summary>
+        /// <param name="articleId">The unique identifier for the article.</param>
+        /// <param name="title">The title of the article.</param>
+        /// <param name="summary">A brief summary of the article.</param>
+        /// <param name="content">The main content of the article.</param>
+        /// <param name="publishedOn">The date and time the article was published.</param>
+        public BaseArticle(
+            int articleId,
+            string title,
+            string summary,
+            string content,
+            DateTime publishedOn)
+        {
+            ArticleId = articleId;
+            Title = title;
+            Summary = summary;
+            Content = content;
+            PublishedOn = publishedOn;
+        }
+
+        /// <summary>
+        /// Gets or sets the unique identifier for the article.
+        /// </summary>
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ArticleId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the title of the article.
+        /// </summary>
+        [Required]
+        [MaxLength(64)]
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Gets or sets a brief summary of the article.
+        /// </summary>
+        [Required]
+        [MaxLength(64)]
+        public string Summary { get; set; }
+
+        /// <summary>
+        /// Gets or sets the main content of the article.
+        /// </summary>
+        [MaxLength(256)]
+        public string Content { get; set; }
+
+        /// <summary>
+        /// Gets or sets the date and time the article was published.
+        /// </summary>
+        [Required]
+        [DataType(DataType.DateTime)]
+        public DateTime PublishedOn { get; set; }
+
+        /// <summary>
+        /// Gets or sets the list of stocks related to the article.
+        /// </summary>
+        public List<BaseStock> RelatedStocks { get; set; } = [];
+    }
+}
---
+++ BankApi/Models/Articles/UserArticle.cs
@@ -0,0 +1,66 @@
+﻿namespace BankApi.Models.Articles
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using BankApi.Models;
+    using BankApi.Validators;
+
+    /// <summary>
+    /// Represents an article created by a user.
+    /// </summary>
+    public class UserArticle : BaseArticle
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UserArticle"/> class.
+        /// </summary>
+        /// <param name="articleId">The unique identifier of the article.</param>
+        /// <param name="title">The title of the article.</param>
+        /// <param name="summary">A brief summary of the article.</param>
+        /// <param name="content">The content of the article.</param>
+        /// <param name="publishedOn">The date and time the article was published.</param>
+        /// <param name="authorId">The unique identifier of the author.</param>
+        /// <param name="topic">The topic of the article.</param>
+        /// <param name="status">The publication status of the article.</param>
+        public UserArticle(
+            int articleId,
+            string title,
+            string summary,
+            string content,
+            DateTime publishedOn,
+            int authorId,
+            string topic,
+            Status status = Status.Pending)
+            : base(articleId, title, summary, content, publishedOn)
+        {
+            AuthorId = authorId;
+            Topic = topic;
+            Status = status;
+        }
+
+        /// <summary>
+        /// Gets or sets the unique identifier of the author.
+        /// </summary>
+        [ForeignKey(nameof(AuthorId))]
+        public int AuthorId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the topic of the article.
+        /// </summary>
+        [Required]
+        [MaxLength(32)]
+        public string Topic { get; set; }
+
+        /// <summary>
+        /// Gets or sets the publication status of the article.
+        /// </summary>
+        [Required]
+        [StatusValidation]
+        public Status Status { get; set; }
+
+        /// <summary>
+        /// Gets or sets the author of the article.
+        /// </summary>
+        public User Author { get; set; } = new();
+    }
+}
