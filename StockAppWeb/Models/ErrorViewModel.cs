06fbc5d6d97d03f7631b90745c0ddabc3bbfcec8
Pascu-Victor <me@vpascu.ro>
2025-05-21 06:28:12 +0300

    Initial webapp project creation

---
+++ StockAppWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StockAppWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
