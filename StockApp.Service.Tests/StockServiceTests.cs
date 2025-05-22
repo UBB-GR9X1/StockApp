c4f396577ffabc5468e651266d47971f8f7e6ffe
NimigeanValentin <nimigeanvalentinoficial@gmail.com>
2025-05-22 15:45:25 +0300

    added tests for stock , store and tips service

---
+++ StockApp.Service.Tests/StockServiceTests.cs
@@ -0,0 +1,200 @@
+﻿using BankApi.Repositories;
+using BankApi.Services;
+using Common.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace StockApp.Service.Tests;
+[TestClass]
+public class StockServiceTests
+{
+    private Mock<IStockRepository> stockRepoMock;
+    private Mock<IHomepageStockRepository> homepageRepoMock;
+    private StockService stockService;
+
+    [TestInitialize]
+    public void Setup()
+    {
+        stockRepoMock = new Mock<IStockRepository>();
+        homepageRepoMock = new Mock<IHomepageStockRepository>();
+
+        stockService = new StockService(stockRepoMock.Object, homepageRepoMock.Object);
+    }
+
+    [TestMethod]
+    public async Task CreateStockAsync_ValidStock_ReturnsCreatedStock()
+    {
+        var stock = new Stock { Id = 1, Name = "TestStock" };
+        stockRepoMock.Setup(r => r.CreateAsync(stock)).ReturnsAsync(stock);
+
+        var result = await stockService.CreateStockAsync(stock);
+
+        Assert.AreEqual(stock, result);
+        stockRepoMock.Verify(r => r.CreateAsync(stock), Times.Once);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentNullException))]
+    public async Task CreateStockAsync_NullStock_ThrowsArgumentNullException()
+    {
+        await stockService.CreateStockAsync(null);
+    }
+
+    [TestMethod]
+    public async Task DeleteStockAsync_ExistingId_ReturnsTrue()
+    {
+        stockRepoMock.Setup(r => r.DeleteAsync(1)).ReturnsAsync(true);
+
+        var result = await stockService.DeleteStockAsync(1);
+
+        Assert.IsTrue(result);
+        stockRepoMock.Verify(r => r.DeleteAsync(1), Times.Once);
+    }
+
+    [TestMethod]
+    public async Task GetAllStocksAsync_ReturnsListOfStocks()
+    {
+        var stocks = new List<Stock> { new Stock { Id = 1 }, new Stock { Id = 2 } };
+        stockRepoMock.Setup(r => r.GetAllAsync()).ReturnsAsync(stocks);
+
+        var result = await stockService.GetAllStocksAsync();
+
+        CollectionAssert.AreEqual(stocks, result.ToList());
+    }
+
+    [TestMethod]
+    public async Task GetStockByIdAsync_ExistingId_ReturnsStock()
+    {
+        var stock = new Stock { Id = 1 };
+        stockRepoMock.Setup(r => r.GetByIdAsync(1)).ReturnsAsync(stock);
+
+        var result = await stockService.GetStockByIdAsync(1);
+
+        Assert.AreEqual(stock, result);
+    }
+
+    [TestMethod]
+    public async Task UpdateStockAsync_ValidInput_ReturnsUpdatedStock()
+    {
+        var updatedStock = new Stock { Id = 1, Name = "Updated" };
+        stockRepoMock.Setup(r => r.UpdateAsync(1, updatedStock)).ReturnsAsync(updatedStock);
+
+        var result = await stockService.UpdateStockAsync(1, updatedStock);
+
+        Assert.AreEqual(updatedStock, result);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentNullException))]
+    public async Task UpdateStockAsync_NullUpdatedStock_ThrowsArgumentNullException()
+    {
+        await stockService.UpdateStockAsync(1, null);
+    }
+
+    [TestMethod]
+    public async Task UserStocksAsync_ValidCnp_ReturnsUserStocks()
+    {
+        var cnp = "1234567890";
+        var userStocks = new List<Stock> { new Stock { Id = 1 }, new Stock { Id = 2 } };
+        stockRepoMock.Setup(r => r.UserStocksAsync(cnp)).ReturnsAsync(userStocks);
+
+        var result = await stockService.UserStocksAsync(cnp);
+
+        CollectionAssert.AreEqual(userStocks, result);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public async Task UserStocksAsync_EmptyCnp_ThrowsArgumentException()
+    {
+        await stockService.UserStocksAsync("");
+    }
+
+    [TestMethod]
+    public async Task GetFilteredAndSortedStocksAsync_FiltersAndSortsCorrectly()
+    {
+        var userCnp = "user1";
+        var stocks = new List<HomepageStock>
+        {
+            new HomepageStock { Id = 1, IsFavorite = true, Change = 2, StockDetails = new Stock { Name = "AAA", Symbol = "AAA", Price = 10 } },
+            new HomepageStock { Id = 2, IsFavorite = false, Change = -1, StockDetails = new Stock { Name = "BBB", Symbol = "BBB", Price = 20 } },
+            new HomepageStock { Id = 3, IsFavorite = true, Change = 5, StockDetails = new Stock { Name = "CCC", Symbol = "CCC", Price = 15 } },
+        };
+
+        homepageRepoMock.Setup(r => r.GetAllAsync(userCnp)).ReturnsAsync(stocks);
+
+        // Test filter by query "A" and favorites only = true, sorted by name
+        var result = await stockService.GetFilteredAndSortedStocksAsync("A", "Sort by Name", true, userCnp);
+
+        Assert.AreEqual(1, result.Count);
+        Assert.AreEqual("AAA", result[0].StockDetails.Name);
+
+        // Test no filter, sort by Price
+        result = await stockService.GetFilteredAndSortedStocksAsync("", "Sort by Price", false, userCnp);
+        Assert.AreEqual(3, result.Count);
+        Assert.AreEqual("AAA", result[0].StockDetails.Name);
+        Assert.AreEqual("CCC", result[1].StockDetails.Name);
+        Assert.AreEqual("BBB", result[2].StockDetails.Name);
+    }
+
+    [TestMethod]
+    public async Task AddToFavoritesAsync_ValidStock_SetsIsFavoriteTrueAndUpdates()
+    {
+        var stock = new HomepageStock { Id = 1, IsFavorite = false };
+        homepageRepoMock.Setup(r => r.UpdateAsync(stock.Id, stock)).ReturnsAsync(true);
+
+        await stockService.AddToFavoritesAsync(stock);
+
+        Assert.IsTrue(stock.IsFavorite);
+        homepageRepoMock.Verify(r => r.UpdateAsync(stock.Id, stock), Times.Once);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentNullException))]
+    public async Task AddToFavoritesAsync_NullStock_ThrowsArgumentNullException()
+    {
+        await stockService.AddToFavoritesAsync(null);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(InvalidOperationException))]
+    public async Task AddToFavoritesAsync_UpdateFails_ThrowsInvalidOperationException()
+    {
+        var stock = new HomepageStock { Id = 1, IsFavorite = false };
+        homepageRepoMock.Setup(r => r.UpdateAsync(stock.Id, stock)).ReturnsAsync(false);
+
+        await stockService.AddToFavoritesAsync(stock);
+    }
+
+    [TestMethod]
+    public async Task RemoveFromFavoritesAsync_ValidStock_SetsIsFavoriteFalseAndUpdates()
+    {
+        var stock = new HomepageStock { Id = 1, IsFavorite = true };
+        homepageRepoMock.Setup(r => r.UpdateAsync(stock.Id, stock)).ReturnsAsync(true);
+
+        await stockService.RemoveFromFavoritesAsync(stock);
+
+        Assert.IsFalse(stock.IsFavorite);
+        homepageRepoMock.Verify(r => r.UpdateAsync(stock.Id, stock), Times.Once);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentNullException))]
+    public async Task RemoveFromFavoritesAsync_NullStock_ThrowsArgumentNullException()
+    {
+        await stockService.RemoveFromFavoritesAsync(null);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(InvalidOperationException))]
+    public async Task RemoveFromFavoritesAsync_UpdateFails_ThrowsInvalidOperationException()
+    {
+        var stock = new HomepageStock { Id = 1, IsFavorite = true };
+        homepageRepoMock.Setup(r => r.UpdateAsync(stock.Id, stock)).ReturnsAsync(false);
+
+        await stockService.RemoveFromFavoritesAsync(stock);
+    }
+}
---
+++ StockApp.Service.Tests/StoreServiceTests.cs
@@ -0,0 +1,150 @@
+﻿using BankApi.Services;
+using BankApi.Repositories;
+using Common.Exceptions;
+using Common.Models;
+using Common.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Threading.Tasks;
+
+namespace StockApp.Service.Tests
+{
+    [TestClass]
+    public class StoreServiceTests
+    {
+        private Mock<IGemStoreRepository> _storeRepoMock;
+        private Mock<IUserRepository> _userRepoMock;
+
+        private const string ValidUserCNP = "1234567890123";
+        private const string ValidAccountId = "acc-001";
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _storeRepoMock = new Mock<IGemStoreRepository>();
+            _userRepoMock = new Mock<IUserRepository>();
+        }
+
+        private StoreService CreateService(bool transactionShouldSucceed = true)
+        {
+            return new TestableStoreService(_storeRepoMock.Object, _userRepoMock.Object, transactionShouldSucceed);
+        }
+
+        private class TestableStoreService : StoreService
+        {
+            private readonly bool _transactionResult;
+
+            public TestableStoreService(IGemStoreRepository repo, IUserRepository userRepo, bool transactionResult)
+                : base(repo, userRepo)
+            {
+                _transactionResult = transactionResult;
+            }
+
+            protected override Task<bool> ProcessBankTransaction(string accountId, double amount)
+            {
+                return Task.FromResult(_transactionResult);
+            }
+        }
+
+        [TestMethod]
+        public async Task GetUserGemBalanceAsync_ReturnsCorrectBalance()
+        {
+            // Arrange
+            _storeRepoMock.Setup(r => r.GetUserGemBalanceAsync(ValidUserCNP)).ReturnsAsync(150);
+            var service = CreateService();
+
+            // Act
+            var balance = await service.GetUserGemBalanceAsync(ValidUserCNP);
+
+            // Assert
+            Assert.AreEqual(150, balance);
+        }
+
+        [TestMethod]
+        public async Task UpdateUserGemBalanceAsync_CallsRepository()
+        {
+            // Arrange
+            var service = CreateService();
+
+            // Act
+            await service.UpdateUserGemBalanceAsync(200, ValidUserCNP);
+
+            // Assert
+            _storeRepoMock.Verify(r => r.UpdateUserGemBalanceAsync(ValidUserCNP, 200), Times.Once);
+        }
+
+        [TestMethod]
+        public async Task BuyGems_HappyPath_UpdatesGemBalance()
+        {
+            // Arrange
+            var deal = new GemDeal("Gold Pack", 50, 10.0, false);
+            _storeRepoMock.Setup(r => r.GetUserGemBalanceAsync(ValidUserCNP)).ReturnsAsync(100);
+            var service = CreateService();
+
+            // Act
+            var result = await service.BuyGems(deal, ValidAccountId, ValidUserCNP);
+
+            // Assert
+            _storeRepoMock.Verify(r => r.UpdateUserGemBalanceAsync(ValidUserCNP, 150), Times.Once);
+            Assert.AreEqual("Successfully purchased 50 gems for 10€", result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(GemTransactionFailedException))]
+        public async Task BuyGems_WhenBankTransactionFails_ThrowsGemTransactionFailedException()
+        {
+            // Arrange
+            var deal = new GemDeal("Gold Pack", 50, 10.0, false);
+            var service = CreateService(transactionShouldSucceed: false);
+
+            // Act
+            await service.BuyGems(deal, ValidAccountId, ValidUserCNP);
+
+            // Assert: exception is expected
+        }
+
+        [TestMethod]
+        public async Task SellGems_HappyPath_UpdatesGemBalance()
+        {
+            // Arrange
+            _storeRepoMock.Setup(r => r.GetUserGemBalanceAsync(ValidUserCNP)).ReturnsAsync(100);
+            var service = CreateService();
+
+            // Act
+            var result = await service.SellGems(50, ValidAccountId, ValidUserCNP);
+
+            // Assert
+            _storeRepoMock.Verify(r => r.UpdateUserGemBalanceAsync(ValidUserCNP, 50), Times.Once);
+            Assert.AreEqual("Successfully sold 50 gems for 0.5€", result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InsufficientGemsException))]
+        public async Task SellGems_WhenNotEnoughGems_ThrowsInsufficientGemsException()
+        {
+            // Arrange
+            _storeRepoMock.Setup(r => r.GetUserGemBalanceAsync(ValidUserCNP)).ReturnsAsync(30);
+            var service = CreateService();
+
+            // Act
+            await service.SellGems(50, ValidAccountId, ValidUserCNP);
+
+            // Assert: exception is expected
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(GemTransactionFailedException))]
+        public async Task SellGems_WhenBankTransactionFails_ThrowsGemTransactionFailedException()
+        {
+            // Arrange
+            _storeRepoMock.Setup(r => r.GetUserGemBalanceAsync(ValidUserCNP)).ReturnsAsync(100);
+            var service = CreateService(transactionShouldSucceed: false);
+
+            // Act
+            await service.SellGems(50, ValidAccountId, ValidUserCNP);
+
+            // Assert: exception is expected
+        }
+    }
+}
